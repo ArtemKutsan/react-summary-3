a78ffd4d3a22ac0c4948d9a58988e32b619d4049
Artem Kutsan <artemkutsan@gmail.com>
2026-02-13 10:45:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// src/App.jsx
+import { useState } from 'react';
+import './App.css';
+// import UserProfile from './components/UserProfile';
+import Paginator from './components/Paginator';
+import DogGallery from './components/DogGallery';
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="container">React Summary 3</h1>
+
+        <section>
+          <div className="container">
+            {/* <h2>Задача 1: Пагинация</h2> */}
+            {/* <Paginator /> */}
+
+            {/* <h2>Задача 2: Галерея собак</h2> */}
+            <DogGallery />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
